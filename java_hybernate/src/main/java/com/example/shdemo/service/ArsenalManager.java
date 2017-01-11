ac981b9f0fedfe236981b4940476ce5e5d3ea48d
Unknown <luszukpawel@gmail.com>
2017-01-11 06:55:02 +0100

    add Arsenal Manager interface + empty methods

--- java_hybernate/src/main/java/com/example/shdemo/service/ArsenalManager.java
+++ java_hybernate/src/main/java/com/example/shdemo/service/ArsenalManager.java
@@ -3,9 +3,28 @@ package com.example.shdemo.service;
 import java.util.List;
 
 import com.example.shdemo.domain.Ammunition;
+import com.example.shdemo.domain.Ammunition;
+import com.example.shdemo.domain.Weapon;
 import com.example.shdemo.domain.Weapon;
 
 public interface ArsenalManager 
 {
-
+	// add implementation 
+	
+	void addWeapon(Weapon Weapon);
+	List<Weapon> getAllWeapons();
+	void deleteWeapon(Weapon Weapon);
+	Weapon findWeaponbyId(Long id);
+	public Weapon findWeaponbyName(String name);
+	public boolean editWeapon(Weapon Weapon);
+	
+	List<Ammunition> getOwnedAmmunitions(Weapon Weapon);
+	List<Ammunition> getFreeAmmunitions();
+	void sellAmmunition(Long WeaponId, Long AmmunitionId);
+	
+	Long addNewAmmunition(Ammunition Ammunition);
+	List<Ammunition> getAllAmmunitions();
+	void deleteAmmunition(Ammunition Ammunition);
+	Ammunition findAmmunitionById(Long id);
+	public boolean editAmmunition(Ammunition Ammunition);
 }
--- java_hybernate/src/main/java/com/example/shdemo/service/ArsenalManagerHibernateImpl.java
+++ java_hybernate/src/main/java/com/example/shdemo/service/ArsenalManagerHibernateImpl.java
@@ -16,5 +16,85 @@ import com.example.shdemo.domain.Weapon;
 public class ArsenalManagerHibernateImpl implements ArsenalManager
 {
 	
+		
+		@Autowired
+		private SessionFactory sessionFactory;
+		
+		public void setSessionFactory(SessionFactory _sessionFactory){
+			this.sessionFactory = _sessionFactory;
+		}
+
+		
+		
+		public void addWeapon(Weapon Weapon) {
+			
+		}
+		
+		@SuppressWarnings("unchecked")
+		public List<Weapon> getAllWeapons() {
+			return null;
+		}
+
+		public void deleteWeapon(Weapon Weapon) {
+
+		}
+
+		public Weapon findWeaponbyId(Long id) {
+			return null;
+		}
+		
+		public Weapon findWeaponbyName(String name) {
+		
+				return null;
+			
+		}
+		
+		public boolean editWeapon(Weapon Weapon){
+		
+			return true;
+		}
+		
+		
+		//Ammunition
+
+		public Long addNewAmmunition(Ammunition Ammunition) {
+			
+			return null;
+			
+		}
+
+		@SuppressWarnings("unchecked")
+		public List<Ammunition> getAllAmmunitions() {
+			return null;
+		}
+
+		public void deleteAmmunition(Ammunition Ammunition) {
+
+		}
+
+		public Ammunition findAmmunitionById(Long id) {
+			return null;
+		}
+
+		public List<Ammunition> getOwnedAmmunitions(Weapon Weapon) {
+	;
+			return null;
+		}
+		
+		@SuppressWarnings("unchecked")
+		public List<Ammunition> getFreeAmmunitions() {
+			return null;
+		}
+		
+		public boolean editAmmunition(Ammunition Ammunition){
+		
+			return true;
+		}
+
+		public void sellAmmunition(Long WeaponId, Long AmmunitionId) {
 	
-}
+		}
+	}
+
+	
+
